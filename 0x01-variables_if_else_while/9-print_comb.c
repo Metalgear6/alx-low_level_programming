68110b91ac0b25dbd6abfa6aa340117725d67438
Mohammed <mohamednew89@gmail.com>
2023-03-05 06:57:42 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Disc: Numbers must be separated by ,, followed by a space
+  *Return: always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
